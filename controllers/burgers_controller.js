657c273b51c29b1eacf15893a5d6ced3e70473ee
Joe Finny <finne132@umn.edu>
2018-05-22 15:35:29 -0500

    created handlebars index/home view and added rest of folder structure - also dropped in a burger image file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,5 +7,45 @@ var router = express.Router();
 
 // create the actual routes
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/", function(req, res) {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], function() {
+      res.redirect("/");
+    });
+  });
+  
+  router.put("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function() {
+      res.redirect("/");
+    });
+  });
+  
+  router.delete("/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function() {
+      res.redirect("/");
+    });
+  });
+
 // export router
 module.exports = router;
