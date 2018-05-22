8a9971671e3577d41c70e2d1b33074bd79551f48
Joe Finny <finne132@umn.edu>
2018-05-22 14:34:45 -0500

    added code to call ORM functions with the burger specific orm inputs

--- models/burger.js
+++ models/burger.js
@@ -3,5 +3,29 @@
 // Import the ORM 
 var orm = require("../config/orm.js");
 
+// call the ORM functions using burger specific input for the ORM
+var burger = {
+    selectAll: function(cb){
+      orm.selectAll('burgers', function(res){
+        cb(res);
+      });
+    },
+    insertOne: function(val, cb){
+      orm.insertOne('burgers', 'burger_name', val, function(res){
+        cb(res);
+      });
+    },
+    updateOne: function(colVal, conditionVal, cb){
+      orm.updateOne('burgers', 'devoured', colVal, 'id', conditionVal, function(res){
+        cb(res);
+      });
+    },
+    deleteOne: function(conditionVal, cb){
+      orm.deleteOne('burgers', 'id', conditionVal, function(res){
+        cb(res);
+      });
+    }
+  };
+
 // Export the database functions
 module.exports = burger;
