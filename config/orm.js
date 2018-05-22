37a2e615cbb52d74c80bdc52161c87c8d5f32845
Joe Finny <finne132@umn.edu>
2018-05-22 16:01:47 -0500

    re-did the orm syntax because the sql just wasnt playing nice, added a helper function to sql-if-y objects

--- config/orm.js
+++ config/orm.js
@@ -4,36 +4,76 @@
 var connection = require("../config/connection.js");
 
 // In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
-var orm ={
-    all: function(table, callback){
-      var queryString = 'SELECT * FROM ' + table;
+function printQuestionMarks(num) {
+    var arr = [];
   
-      connection.query(queryString, function(err, data){
-        if(err) throw err;
-        callback(data);
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  // convers objects to something sql can understand. this prevents the "ER_PARSE_ERROR" you get from having [object Object] in your SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+    for (var i in ob) {
+      if (Object.hasOwnProperty.call(ob, i)) {
+        arr.push(i + "=" + ob[i]);
+      }
+    }
+    return arr.toString();
+  }
+  
+  // the actual orm object to hold all the sql functions all of this syntax had to be re-done to work 
+  var orm = {
+    all: function(tableInput, cb) {
+      var queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
       });
     },
-
-    // insertOne function - create 1 new burger with the input parameters)
-    create: function(table, column, burgerInput, callback){
-        var queryString = 'INSERT INTO ' + table + '(' + column + ') VALUES (?)';
-
-        connection.query(queryString, [burgerInput], function(err, data){
-        if(err) throw err;
-        callback(data);
-        });
+    create: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+  
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
     },
-
-    // update an existing burger to show that it was eaten
-    update: function(table, col, colVal, condition, conditionVal, callback){
-        var queryString = 'UPDATE ' + table + ' SET ' + col + '=?' + 'WHERE ' + condition + '=?';
-
-        connection.query(queryString, [colVal, conditionVal], function(err, data){
-        if(err) throw err;
-        callback(data);
-        });
+    // An example of objColVals would be {name: panther, sleepy: true}
+    update: function(table, objColVals, condition, cb) {
+      var queryString = "UPDATE " + table;
+  
+      queryString += " SET ";
+      queryString += objToSql(objColVals);
+      queryString += " WHERE ";
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        cb(result);
+      });
     }
-};
+  };
 
 // Export the ORM object in module.exports
 module.exports=orm;
